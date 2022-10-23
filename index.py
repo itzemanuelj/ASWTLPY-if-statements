d7993b01b88adeef2b13933c5e6a54acf15c49f1
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-23 01:40:41 -0400

    mvp commit for 1f statements in python

---
+++ index.py
@@ -0,0 +1,79 @@
+x=5
+if x == 5:
+  print("x is 5")
+else:
+  print("x is not 5")
+# double equal
+
+# Rewrite this line so it's valid:
+species = "cat"
+if species == "cat":
+    print("Its a cat")
+else:
+    print("other")
+
+# Rewrite this line so it's valid:
+if species == "cat":
+  print("species is a cat")
+else:
+  print("other species")
+
+# Write the first line of an if statement that tests whether species is "cat"        
+if species == "cat":
+  print("species is a cat")
+else:
+  print("other species")
+
+# Code the first line of an if statement that tests whether x has the same value as y
+x = 1
+y = 1
+if x == y:
+  print("x is equal to y")
+else:
+  print("x is other")
+
+#  Code the first line of an if statement that tests whether one variable is the same as another. Make up the variable names. 
+var_one = 10
+var_two = 10
+
+if var_one == var_two:
+  print("vars are equal")
+else:
+  print("vars are not equal")
+
+# Below, I've coded the first line of an if statement. Code the second line. The second line tells Python to display the string "OK". Don't forget to indent the second line.
+current_address = 100001
+previous_address = 1000002
+
+if current_address == previous_address: 
+  print("OK")
+else:
+  print("not ok")
+
+# If total has the value of 100, tax has the value of 2. Code both lines. Don't forget to indent the second line. 
+total = 100
+if total == 100:
+  tax =2
+
+# If first_name is "Sherlock" last_name is "Holmes" and pal is "Watson". Code all three lines.  
+first_name = "Sherlock"
+
+if first_name == "Sherlock ":
+  last_name = "Holmes"
+  pal = "Watson"
+  print(first_name + last_name + "pal is " + pal)
+
+# If you_won is "yes" tell Python to display "congrats". Code both lines.
+yes = "yes"
+
+if yes == "yes":
+  print("congrats")
+else:
+  print("you did not win")
+
+# Correct the mistakes on line 2.
+
+human = "species"
+species = "human"
+if species == human:
+  print("correct")  
